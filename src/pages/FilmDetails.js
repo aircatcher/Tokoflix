4514bba26a2a52440b7510eefab0693a4468a46d
Ferick Andrew <ferickwar@gmail.com>
2018-06-29 20:53:26 +0700

    Initial setup

---
+++ src/pages/FilmDetails.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class FilmDetails extends React.Component
+{
+  render()
+  {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default FilmDetails;
